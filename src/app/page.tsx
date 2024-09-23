b3b556d8c6544f3bf5897583e5808fb55eab8179
mazmann <sadi.lundberg@hotmail.com>
2024-09-23 13:51:42 +0200

    animating header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,105 @@ export default function Home() {
   return (
     <div>
       <main>
-    
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
+    <div>Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler Lorem ipsum doler 
+    </div>
       </main>
     
     </div>
